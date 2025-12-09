56fcd6c7ea02bb5aed5dfc164a84e56a3591310e
arlaspresident <jankovic4901@gmail.com>
2025-12-09 19:27:03 +0200

    förbereder för render

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 import Hapi from '@hapi/hapi';
-import db from "./database.js";
-import registerWorkoutRoutes from "./routes/workoutsRoutes.js";
-
+import db from './database.js';
+import registerWorkoutRoutes from './routes/workoutsRoutes.js';
+import cors from '@hapi/cors';
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost',
-    });
+  const server = Hapi.server({
+    port: process.env.PORT || 3000,   
+    host: '0.0.0.0',                 
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: () => {
-            return { message: 'Hello World' };
+  await server.register({
+    plugin: cors,
+    options: {
+      origins: ['*'],
+    },
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => {
+      return { message: 'Hello World' };
     },
   });
 
   registerWorkoutRoutes(server);
 
   await server.start();
-  console.log('Server running on http://localhost:3000');
-
+  console.log(`Server running on ${server.info.uri}`);
 };
 
 init();
